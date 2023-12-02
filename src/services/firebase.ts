31134017739b71451384d782a828d63cc109bbb6
Turbovv <108888834+Turbovv@users.noreply.github.com>
2023-12-02 16:43:12 +0400

    fix: firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBtbIwkPrebH-13HmCeZLRcnW88AIMkxvE',
-  authDomain: 'postonce-981ea.firebaseapp.com',
-  projectId: 'postonce-981ea',
-  storageBucket: 'postonce-981ea.appspot.com',
-  messagingSenderId: '660548938561',
-  appId: '1:660548938561:web:4376cf14ba489623278fc0',
+  apiKey: 'AIzaSyCrwNtygod6wC3dwnc7MGhwsVkUrc_AQ_k',
+  authDomain: 'postonce-ed9fb.firebaseapp.com',
+  projectId: 'postonce-ed9fb',
+  storageBucket: 'postonce-ed9fb.appspot.com',
+  messagingSenderId: '594951104884',
+  appId: '1:594951104884:web:c2b763709e04e6bd8c2421',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
