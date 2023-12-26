024c9522054ea1166a23f24744cd3c72cf9aea43
Turbovv <giga.elizbarashvili13@gmail.com>
2023-12-26 22:47:41 +0400

    fix: linter errors

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCrwNtygod6wC3dwnc7MGhwsVkUrc_AQ_k',
@@ -10,5 +11,6 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
 
-export default firebaseApp;
+export  { firebaseApp, firestore };
