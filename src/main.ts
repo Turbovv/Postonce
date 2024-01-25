a466301475d3fbfa76001b4a8e5928ac7ad4f42c
Turbovv <giga.elizbarashvili13@gmail.com>
2024-01-25 15:36:03 +0400

    remove: font package

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import { loadFonts } from './plugins/webfontloader';
 import router from './routes';
 
 import { createPinia } from 'pinia';
 
-loadFonts();
 
 const app = createApp(App);
 
