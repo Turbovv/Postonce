9be0fbd9054709a4e4ea6a9b7d1441c3089fe59f
Turbovv <giga.elizbarashvili13@gmail.com>
2024-01-29 13:54:21 +0400

    add: google icon

--- src/components/GoogleAuth/GoogleAuth.vue
+++ src/components/GoogleAuth/GoogleAuth.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1 v-if="!authStore.isAuthenticated">Wanna Authorize?</h1>
-    <button @click="signInWithGoogle" v-if="!authStore.isAuthenticated">Authorize with Google</button>
+    <button @click="signInWithGoogle" v-if="!authStore.isAuthenticated">Authorize with Google 
+      <img class="google-button"  width="22" height="22" src="https://img.icons8.com/color/33/google-logo.png" alt="google-logo"/>
+    </button>
     <div v-if="authStore.isAuthenticated">
       <p>{{ authStore.user.displayName }}</p>
       <img :src="authStore.user.photoURL" alt="Profile Picture" />
@@ -22,7 +24,6 @@ export default {
   methods: {
     async signInWithGoogle() {
       await this.authStore.signInWithGoogle();
-      this.$router.push("/create")
     },
     async signOut() {
       await this.authStore.signOut();
@@ -33,3 +34,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.google-button {
+  transform: translateY(5px);
+}
+</style>
