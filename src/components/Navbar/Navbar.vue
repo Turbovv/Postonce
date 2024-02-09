01eb28405d91358b4de78090a2db019c008b86e2
Turbovv <giga.elizbarashvili13@gmail.com>
2024-02-09 20:13:02 +0400

    fix: auth logic

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -33,6 +33,7 @@ export default defineComponent({
                 });
             } else {
                 this.user = null;
+                this.$router.push('/')
             }
         });
     },
